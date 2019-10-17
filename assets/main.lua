7d6729f585c50c23f5231cf83130cc6309c104d2
jdbc <jdbc.dev@gmail.com>
2019-10-17 21:01:47 +0200

    First commmit

---
+++ assets/main.lua
@@ -0,0 +1,36 @@
+
+application:setKeepAwake(true)
+
+-- local client_async = require ("websocket.client_async")
+
+local ColyseusClient = require "colyseus.client"
+
+local client = ColyseusClient.new("ws://localhost:2567")
+if (client) then
+	print("Created client >> Endpoint "..client.hostname);
+	
+	-- join chat room
+    client:join_or_create("chat", {}, function(err, _room)
+		if err then
+			print("JOIN ERROR: " .. err)
+			return
+		end
+		
+		print("Joined to room")
+		room = _room
+    end)
+end
+
+--[[local ws=WebSocket.new("ws://echo.websocket.org")
+ws:send("Stuff")
+print(ws:receive())
+]]--
+
+local function enter_frame() 
+
+	client:loop()
+	--client:loop(1)
+	--print("update")
+end
+
+stage:addEventListener(Event.ENTER_FRAME, enter_frame)
